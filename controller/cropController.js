2fa23c4fd8cc1ae5938511fb578513be46fa50ce
Delvoid <delvoid.dev@gmail.com>
2022-07-12 20:08:50 +0100

    page format and codec change

--- controller/cropController.js
+++ controller/cropController.js
@@ -1,77 +1,86 @@
-import got from 'got'
-import fs from 'fs'
-import FormData from 'form-data'
-import { performance } from 'universal-perf-hooks'
+import got from 'got';
+import fs from 'fs';
+import FormData from 'form-data';
+import { performance } from 'universal-perf-hooks';
 
-import { fileioUpload, makeVideoVertical } from '../service/cropService.js'
+import { fileioUpload, makeVideoVertical } from '../service/cropService.js';
 
-const currentJobStatus = {}
+const currentJobStatus = {};
 
 export const createCropVideo = async (req, res) => {
-  if (!req.headers.authorization) return res.status(400).send()
-  const { APP_KEY } = process.env
-  if (!APP_KEY) return res.status(500).send('internal server error')
-  const ACTION_KEY = req.headers.authorization.split(' ')[1]
-  if (ACTION_KEY !== APP_KEY) return res.status(400).send()
+  if (!req.headers.authorization) return res.status(400).send();
+  const { APP_KEY } = process.env;
+  if (!APP_KEY) return res.status(500).send('internal server error');
+  const ACTION_KEY = req.headers.authorization.split(' ')[1];
+  if (ACTION_KEY !== APP_KEY) return res.status(400).send();
 
-  const { clip, cropData } = req.body
-  if (!clip || !cropData) return res.status(400).send('please provide both clip and crop data')
+  const { clip, cropData } = req.body;
+  console.log('Clip Data');
+  console.log(clip);
+  console.log('cropData');
+  console.log(cropData);
 
-  const downStart = performance.now()
-  const { download_url: downloadUrl, id } = clip
-  let fileStream = got.stream(downloadUrl)
-  const fileName = `${Math.random().toString(36).substring(2, 15)}_${id}.mp4`
+  if (!clip || !cropData) return res.status(400).send('please provide both clip and crop data');
 
-  const downEnd = performance.now()
-  console.log(`DOWNLOAD call took ${downEnd - downStart} ms`)
+  const downStart = performance.now();
+  const { id } = clip;
+  const downloadUrl = clip.download_url || clip.downloadUrl;
+  let fileStream = got.stream(downloadUrl);
+  const fileName = `${Math.random().toString(36).substring(2, 15)}_${id}.mp4`;
 
-  currentJobStatus[fileName] = { status: 'processing' }
-  res.status(200).send({ id: fileName, ...currentJobStatus[fileName] })
+  const downEnd = performance.now();
+  console.log(`DOWNLOAD call took ${downEnd - downStart} ms`);
+
+  currentJobStatus[fileName] = { status: 'processing' };
+  res.status(200).send({ id: fileName, ...currentJobStatus[fileName] });
 
   //wait for filestream to end
-  console.log('streaming')
-  const streamStart = performance.now()
+  console.log('streaming');
+  const streamStart = performance.now();
   await new Promise((resolve, reject) => {
-    let file = fs.createWriteStream('./' + fileName)
-    fileStream.pipe(file)
+    let file = fs.createWriteStream('./' + fileName);
+    fileStream.pipe(file);
     file.on('finish', () => {
-      resolve()
-    })
-  })
-  const streamEnd = performance.now()
-  console.log(`Stream call took ${streamEnd - streamStart} ms`)
+      resolve();
+    });
+    file.on('error', (err) => {
+      console.log('GOOFED');
+    });
+  });
+  const streamEnd = performance.now();
+  console.log(`Stream call took ${streamEnd - streamStart} ms`);
 
-  const verticalStart = performance.now()
-  const editVideo = await makeVideoVertical(clip, cropData, fileName)
-  const verticalEnd = performance.now()
-  console.log(`makeVideoVertical call took ${verticalEnd - verticalStart} ms`)
+  const verticalStart = performance.now();
+  const editVideo = await makeVideoVertical(clip, cropData, fileName);
+  const verticalEnd = performance.now();
+  console.log(`makeVideoVertical call took ${verticalEnd - verticalStart} ms`);
 
-  console.log('video edited')
+  console.log('video edited');
 
   //fileIo
-  let form = new FormData()
-  const fileStart = performance.now()
-  form.append('file', fs.createReadStream(`./${editVideo}`))
-  let fileIOResponse = await fileioUpload(form)
+  let form = new FormData();
+  const fileStart = performance.now();
+  form.append('file', fs.createReadStream(`./${editVideo}`));
+  let fileIOResponse = await fileioUpload(form);
 
-  const fileEnd = performance.now()
-  console.log(`Fileio upload call took ${fileEnd - fileStart} ms`)
+  const fileEnd = performance.now();
+  console.log(`Fileio upload call took ${fileEnd - fileStart} ms`);
 
-  console.log('fileio done')
+  console.log('fileio done');
 
   currentJobStatus[fileName] = {
     fileURL: fileIOResponse.data.link,
     key: fileIOResponse.data.key,
     status: 'done',
-  }
+  };
 
   // delete local files
-  fs.unlinkSync(`./${editVideo}`)
-  fs.unlinkSync('./' + fileName)
-  console.log('files deleted: ', editVideo, fileName)
-}
+  fs.unlinkSync(`./${editVideo}`);
+  fs.unlinkSync('./' + fileName);
+  console.log('files deleted: ', editVideo, fileName);
+};
 
 export const videoStatus = async (req, res) => {
-  const jobId = req.query.id
-  res.status(200).send(currentJobStatus[jobId])
-}
+  const jobId = req.query.id;
+  res.status(200).send(currentJobStatus[jobId]);
+};
