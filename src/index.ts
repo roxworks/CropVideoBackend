c06266b328fadd7ec8019f3f882e3ad2e76f2914
Delvoid <delvoid.dev@gmail.com>
2022-09-28 23:08:47 +0100

    add queue dashbaord

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express from 'express';
 import cors from 'cors';
 import { path } from '@ffmpeg-installer/ffmpeg';
 import ffmpeg from 'fluent-ffmpeg';
+
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+
 import * as middlewares from './middlewares';
 import api from './api';
 
@@ -11,11 +16,21 @@ ffmpeg.setFfmpegPath(path);
 // @ts-ignore
 import { path as ffprobePath } from '@ffprobe-installer/ffprobe';
 import MessageResponse from './interfaces/MessageResponse';
+import { clipQueue } from './queues/clip.queue';
 ffmpeg.setFfprobePath(ffprobePath);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: [new BullMQAdapter(clipQueue, { readOnlyMode: true })],
+  serverAdapter: serverAdapter,
+});
+
+// cron jobs
 scheduledFunctions();
 
 app.use(cors());
@@ -28,6 +43,7 @@ app.get<{}, MessageResponse>('/', (req, res) => {
 });
 
 app.use('/', api);
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
