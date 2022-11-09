f4da32035903df1adcb3946cb2dd8e02b3b2d177
Delvoid <delvoid.dev@gmail.com>
2022-11-09 23:05:53 +0000

    add new templates

--- src/api/crop/crop.service.ts
+++ src/api/crop/crop.service.ts
@@ -72,17 +72,6 @@ export const makeVideoVertical = async (clip: Clip, clipSettings: CropData, file
   let multiplier = isNormalized ? 9 / 16 : 1;
   let SHA = roundTo4(Math.ceil(((SWA * CHA) / CWA) * multiplier));
   let HA = SHA;
-  if (cropType === 'freeform') {
-    if (CHA / CWA > 16 / 9) {
-      log('info', 'crop-freeform', `Crop is too tall, forcing to ${OUTPUT_HEIGHT} height`);
-      SHA = OUTPUT_HEIGHT;
-      SWA = roundTo4(Math.ceil((SHA * CWA) / CHA));
-    } else {
-      log('info', 'crop-freeform', `Crop is too wide, forcing to ${OUTPUT_WIDTH} width`);
-      SWA = OUTPUT_WIDTH;
-      SHA = roundTo4(Math.ceil(((SWA * CHA) / CWA) * multiplier));
-    }
-  }
 
   // bottom
   let OW = OUTPUT_WIDTH;
@@ -96,11 +85,22 @@ export const makeVideoVertical = async (clip: Clip, clipSettings: CropData, file
   let SWB = OUTPUT_WIDTH;
   let HB = SHB;
 
+  if (cropType === 'freeform') {
+    if (CHB / CWB > 16 / 9) {
+      log('info', 'crop-freeform', `Crop is too tall, forcing to ${OUTPUT_HEIGHT} height`);
+      SHB = OUTPUT_HEIGHT;
+      SWB = roundTo4(Math.ceil((SHB * CWB) / CHB));
+    } else {
+      log('info', 'crop-freeform', `Crop is too wide, forcing to ${OUTPUT_WIDTH} width`);
+      SWB = OUTPUT_WIDTH;
+      SHB = roundTo4(Math.ceil(((SWB * CHB) / CWB) * multiplier));
+    }
+  }
   if (cropType === 'cam-freeform') {
     if (CHB / CWB > 16 / 9) {
       log('info', 'crop-cam-freeform', `Crop is too tall, forcing to ${OUTPUT_HEIGHT} height`);
       // SHB = 1920 - SHA;
-      SWB = roundTo4(Math.ceil((SHB * CWB) / CHB));
+      SWB = Math.ceil((SHB * CWB) / (CHB * multiplier));
     } else {
       log('info', 'crop-freeform', `Crop is too wide, forcing to ${OUTPUT_WIDTH} width`);
       // SWB = 1080;
@@ -108,15 +108,39 @@ export const makeVideoVertical = async (clip: Clip, clipSettings: CropData, file
     }
   }
 
+  console.log(`CWA: ${CWA}`);
+  console.log(`CHA: ${CHA}`);
+  console.log(`CXA: ${CXA}`);
+  console.log(`CYA: ${CYA}`);
+  console.log(`CWB: ${CWB}`);
+  console.log(`CHB: ${CHB}`);
+  console.log(`CXB: ${CXB}`);
+  console.log(`CYB: ${CYB}`);
+
+  //log all heights and widths
+  console.log('SHA: ' + SHA);
+  console.log('SWA: ' + SWA);
+  console.log('SHB: ' + SHB);
+  console.log('SWB: ' + SWB);
+  console.log('HA: ' + HA);
+  console.log('HB: ' + HB);
+
   let filter = '';
   if (isNormalized) {
     switch (cropType) {
       //Math.ceil(number / 4) * 4
       case 'cam-top':
-      case 'cam-freeform':
         filter = `[0:v]split=2[a][b];
         [a]crop=w=min(ceil(${CWA}*iw/4)*4\\, iw):h=min(ceil(${CHA}*ih/4)*4\\, ih):x=min(ceil(${CXA}*iw/4)*4\\, iw):y=min(ceil(${CYA}*ih/4)*4\\, ih),scale=w=min(${OUTPUT_WIDTH}\\, ceil(${SWA}/4)*4):h=min(${OUTPUT_HEIGHT}\\, ceil(${SHA}/4)*4)[a];
         [b]crop=w=min(ceil(${CWB}*iw/4)*4\\, iw):h=min(ceil(${CHB}*ih/4)*4\\, ih):x=min(ceil(${CXB}*iw/4)*4\\, iw):y=min(ceil(${CYB}*ih/4)*4\\, ih),scale=w=min(${OUTPUT_WIDTH}\\, ceil(${SWB}/4)*4):h=min(${OUTPUT_HEIGHT}\\, ceil(${SHB}/4)*4),pad=w=${OUTPUT_WIDTH}:h=${
+          OUTPUT_HEIGHT - SHA
+        }:x=(ow-iw)/2:y=0:color=black[b];
+        [a][b]vstack`;
+        break;
+      case 'cam-freeform':
+        filter = `[0:v]split=2[a][b];
+        [a]crop=w=min(ceil(${CWA}*iw/4)*4\\, iw):h=min(ceil(${CHA}*ih/4)*4\\, ih):x=min(ceil(${CXA}*iw/4)*4\\, iw):y=min(ceil(${CYA}*ih/4)*4\\, ih),scale=w=min(${OUTPUT_WIDTH}\\, ceil(${SWA}/4)*4):h=min(${OUTPUT_HEIGHT}\\, ceil(${SHA}/4)*4)[a];
+        [b]crop=w=min(ceil(${CWB}*iw)\\, iw):h=min(ceil(${CHB}*ih)\\, ih):x=min(ceil(${CXB}*iw)\\, iw):y=min(ceil(${CYB}*ih)\\, ih),scale=w=min(${OUTPUT_WIDTH}\\, ${SWB}):h=min(${OUTPUT_HEIGHT}\\, ${SHB}),pad=w=${OUTPUT_WIDTH}:h=${
           OUTPUT_HEIGHT - SHA
         }:x=(ow-iw)/2:y=0:color=black[b];
         [a][b]vstack`;
@@ -125,13 +149,13 @@ export const makeVideoVertical = async (clip: Clip, clipSettings: CropData, file
         filter = `crop=w=min(ceil(${CWB}*iw/4)*4\\, iw):h=min(ceil(${CHB}*ih/4)*4\\, ih):x=min(ceil(${CXB}*iw/4)*4\\, iw):y=min(ceil(${CYB}*ih/4)*4\\, ih),scale=w=${OUTPUT_WIDTH}:h=${OUTPUT_HEIGHT}`;
         break;
       case 'freeform':
-        filter = `crop=w=min(ceil(${CWA}*iw/4)*4\\, iw):h=min(ceil(${CHA}*ih/4)*4\\, ih):x=min(ceil(${CXA}*iw/4)*4\\, iw):y=min(ceil(${CYA}*ih/4)*4\\, ih),scale=w=${SWA}:h=${SHA},pad=w=${OUTPUT_WIDTH}:h=${OUTPUT_HEIGHT}:x=(ow-iw)/2:y=(oh-ih)/2`;
+        filter = `crop=w=min(ceil(${CWB}*iw/4)*4\\, iw):h=min(ceil(${CHB}*ih/4)*4\\, ih):x=min(ceil(${CXB}*iw/4)*4\\, iw):y=min(ceil(${CYB}*ih/4)*4\\, ih),scale=w=${SWB}:h=${SHB},pad=w=${OUTPUT_WIDTH}:h=${OUTPUT_HEIGHT}:x=(ow-iw)/2:y=(oh-ih)/2`;
         break;
     }
   } else {
     switch (cropType) {
       case 'cam-top':
-        filter = `[0:v]split=2[a][b];
+        filter = `[0:v]split=2[a][b]; 
         [a]crop=w=${CWB}:h=${CHB}:x=${CXB}:y=${CYB},scale=w=${SWB}:h=${SHB}[d];
         [b]crop=w=${CWA}:h=${CHA}:x=${CXA}:y=${CYA},scale=w=${SWA}:h=${SHA}[c];
         [c][d]vstack`;
