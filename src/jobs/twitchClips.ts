ac9e03fd9803a9cff392d91b349729a8d59de3c2
Delvoid <delvoid.dev@gmail.com>
2022-11-30 19:44:39 +0000

    update default clips status

--- src/jobs/twitchClips.ts
+++ src/jobs/twitchClips.ts
@@ -2,7 +2,11 @@ import cron from 'node-cron';
 
 import { EVERY_MINUTE, EVERY_10_MINUTES } from './cronConstants';
 import { addToGetAllClipsQueue, addToGetLatestClipsQueue } from '../utils/twitch/clips.handler';
-import { getUsersLatestsClips, getUsersWithoutClips } from '../service/User';
+import {
+  getUsersLatestsClips,
+  getUsersWithoutClips,
+  updateUserDefaultClipsById,
+} from '../service/User';
 import log from '../utils/logger';
 
 export default () => {
@@ -19,8 +23,8 @@ export default () => {
       for (const user of users) {
         const twitchProvider = user.accounts?.filter((acc) => acc.provider === 'twitch')[0];
         if (!twitchProvider.providerAccountId) return;
-
-        addToGetAllClipsQueue(user.id, twitchProvider.providerAccountId);
+        await updateUserDefaultClipsById(user.id, 'inqueue');
+        await addToGetAllClipsQueue(user.id, twitchProvider.providerAccountId);
       }
     } catch (error) {
       if (error instanceof Error) {
