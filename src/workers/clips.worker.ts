7576a3af3e9a50fd9fd6a723024404dc0edaaf55
Delvoid <delvoid.dev@gmail.com>
2022-12-29 13:51:35 +0000

    update clips views count

--- src/workers/clips.worker.ts
+++ src/workers/clips.worker.ts
@@ -1,16 +1,24 @@
 import { Job } from 'bullmq';
 import { updateLastUploadDate } from '../service/Settings';
 import { getUserByIdWithAccountsAndSettings } from '../service/User';
-import { getClipsStartingAtCertainDateFromTwitchAPI } from '../utils/twitch/clips.handler';
-import { autoApproveClips, bulkSaveTwitchClips, isClipInDB } from '../service/TwitchClip';
+import {
+  getClipsStartingAtCertainDateFromTwitchAPI,
+  previousXDays,
+} from '../utils/twitch/clips.handler';
+import {
+  autoApproveClips,
+  bulkSaveTwitchClips,
+  isClipInDB,
+  upsertLatestClips,
+} from '../service/TwitchClip';
 import log from '../utils/logger';
 import { UserWithAccountsAndSettingsWithId } from '../api/user/user.model';
+import dayjs from 'dayjs';
 
 const clipsProducer = async (job: Job<{ userId: string; providerAccountId: string }, any, any>) => {
   log('info', 'clip-producer start ', job.data, 'clips.worker');
   const { userId } = job.data;
-
-  // throw new Error('whoops');
+  console.log({ jobName: job.name });
 
   try {
     const user = (await getUserByIdWithAccountsAndSettings(
@@ -48,12 +56,17 @@ const clipsProducer = async (job: Job<{ userId: string; providerAccountId: strin
       };
     }
 
-    // check if auto approve is on
-    if (userSettings?.defaultApprove) {
-      await autoApproveClips(userSettings);
-    }
+    const daysAgo = previousXDays(3);
+    const oldestClipDate =
+      user?.settings?.lastUploaded && daysAgo > new Date(user.settings.lastUploaded).toISOString()
+        ? new Date(user.settings.lastUploaded).toISOString()
+        : daysAgo;
 
-    if (clips.length === 1) {
+    const date1 = dayjs(oldestClipDate);
+    const date2 = dayjs();
+    const diffDays = date2.diff(date1, 'day');
+
+    if (clips.length === 1 && diffDays > 3) {
       const lastUploadedClip = userSettings?.lastUploadedId;
       if (lastUploadedClip === clips[0].twitch_id) {
         return {
@@ -65,30 +78,41 @@ const clipsProducer = async (job: Job<{ userId: string; providerAccountId: strin
       }
     }
 
-    // remove lastUploadedId - to stop duplicated uploads as mongo doesnt allow skipDuplicated check on createMany in bulkSaveTwitchClips
-    if (userSettings?.lastUploadedId) {
-      // clips = clips.filter((clip) => clip.twitch_id !== userSettings.lastUploadedId);
-      const results = await Promise.all(clips.map((clip) => isClipInDB(clip)));
-      const clipsInDb = [];
-      for (let i = 0; i < clips.length; i++) {
-        if (!results[i]) {
-          clipsInDb.push(clips[i]);
+    if (job.name === 'getLatest') {
+      // update latest clips
+      await upsertLatestClips(clips, user.id);
+    } else {
+      // remove lastUploadedId - to stop duplicated uploads as mongo doesnt allow skipDuplicated check on createMany in bulkSaveTwitchClips
+      if (userSettings?.lastUploadedId) {
+        const results = await Promise.all(clips.map((clip) => isClipInDB(clip)));
+        const clipsInDb = [];
+        for (let i = 0; i < clips.length; i++) {
+          if (!results[i]) {
+            clipsInDb.push(clips[i]);
+          }
         }
+        if (clipsInDb.length === 0) {
+          return {
+            name: user.name,
+            broadcasterId: job.data.providerAccountId,
+            clipsCount: clipsInDb.length,
+            autoApprove: userSettings?.defaultApprove,
+          };
+        }
+        clips = clipsInDb;
       }
-      if (clipsInDb.length === 0) {
-        return {
-          name: user.name,
-          broadcasterId: job.data.providerAccountId,
-          clipsCount: clipsInDb.length,
-          autoApprove: userSettings?.defaultApprove,
-        };
-      }
-      clips = clipsInDb;
+
+      // bulk save clips
+      await bulkSaveTwitchClips(clips);
     }
-    await bulkSaveTwitchClips(clips);
+
     const lastUpload = clips[clips.length - 1];
     await updateLastUploadDate(userId, new Date(lastUpload.created_at), lastUpload.twitch_id);
 
+    // check if auto approve is on
+    if (userSettings?.defaultApprove) {
+      await autoApproveClips(userSettings);
+    }
     return {
       name: user.name,
       broadcasterId: job.data.providerAccountId,
